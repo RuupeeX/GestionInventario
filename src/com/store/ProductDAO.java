e84eebc294c84b74a608b2f8ec63dbbe832bd62c
Alejandro <byrupeee@gmail.com>
2025-11-04 09:11:26 +0100

    Comentado

--- src/com/store/ProductDAO.java
+++ src/com/store/ProductDAO.java
@@ -1,8 +1,10 @@
 package com.store;
 
-import com.store.InvalidProductException;
-import com.store.ProductValidator;
-import java.sql.*;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.List;
 
